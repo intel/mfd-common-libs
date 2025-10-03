99958af83dc9ffdd14f58cf19ad8c59833f3b9cd
Mateusz Chrominski <mateusz.chrominski@intel.com>
2025-10-03 12:12:34 +0200

    feat: Option to set log level in DisableLogger.
    Signed-off-by: Mateusz Chrominski <mateusz.chrominski@intel.com>

--- mfd_common_libs/disable_logger.py
+++ mfd_common_libs/disable_logger.py
@@ -9,8 +9,12 @@ from .log_levels import MODULE_DEBUG
 class DisableLogger:
     """Class for context manager temporarily suppressing logging."""
 
+    def __init__(self, level: int = MODULE_DEBUG):
+        """Initialize DisableLogger."""
+        self.level = level
+
     def __enter__(self):
-        logging.disable(MODULE_DEBUG)
+        logging.disable(self.level)
 
     def __exit__(self, exit_type, exit_value, exit_traceback):  # noqa: ANN001
         logging.disable(logging.NOTSET)
